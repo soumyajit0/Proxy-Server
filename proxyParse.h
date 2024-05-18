5fe044dcf4e06d2ec06b815937bb32a66053119d
soumyajit0 <soumyajitdeysarkar@gmail.com>
2024-05-18 20:52:29 +0530

    final commit

--- proxyParse.h
+++ proxyParse.h
@@ -1,5 +1,5 @@
 /*
- * proxy_parse.h -- a HTTP Request Parsing Library.
+ * proxyParse.h -- a HTTP Request Parsing Library.
  *
  * Written by: Matvey Arye
  * For: COS 518 
@@ -30,18 +30,19 @@
    The buf and buflen fields are used internally to maintain the parsed request
    line.
  */
-struct ParsedRequest {
-     char *method; 
-     char *protocol; 
-     char *host; 
-     char *port; 
-     char *path;
-     char *version;
-     char *buf;
-     size_t buflen;
-     struct ParsedHeader *headers;
-     size_t headersused;
-     size_t headerslen;
+struct ParsedRequest
+{
+  char *method;
+  char *protocol;
+  char *host;
+  char *port;
+  char *path;
+  char *version;
+  char *buf;
+  size_t buflen;
+  struct ParsedHeader *headers;
+  size_t headersused;
+  size_t headerslen;
 };
 
 /* 
@@ -49,24 +50,25 @@ struct ParsedRequest {
    format "key:value\r\n" and is maintained in the ParsedHeader linked list
    within ParsedRequest
 */
-struct ParsedHeader {
-     char * key;
-     size_t keylen;
-     char * value;
-     size_t valuelen;
+struct ParsedHeader
+{
+  char *key;
+  size_t keylen;
+  char *value;
+  size_t valuelen;
 };
 
 
 /* Create an empty parsing object to be used exactly once for parsing a single
  * request buffer */
-struct ParsedRequest* ParsedRequest_create();
+struct ParsedRequest *ParsedRequest_create ();
 
 /* Parse the request buffer in buf given that buf is of length buflen */
-int ParsedRequest_parse(struct ParsedRequest * parse, const char *buf,
-			int buflen);
+int ParsedRequest_parse (struct ParsedRequest *parse, const char *buf,
+			 int buflen);
 
 /* Destroy the parsing object. */
-void ParsedRequest_destroy(struct ParsedRequest *pr);
+void ParsedRequest_destroy (struct ParsedRequest *pr);
 
 /* 
    Retrieve the entire buffer from a parsed request object. buf must be an
@@ -74,8 +76,8 @@ void ParsedRequest_destroy(struct ParsedRequest *pr);
    line, headers and the trailing \r\n. buf will not be NUL terminated by
    unparse().
  */
-int ParsedRequest_unparse(struct ParsedRequest *pr, char *buf, 
-			  size_t buflen);
+int ParsedRequest_unparse (struct ParsedRequest *pr, char *buf,
+			   size_t buflen);
 
 /* 
    Retrieve the entire buffer with the exception of request line from a parsed
@@ -84,26 +86,26 @@ int ParsedRequest_unparse(struct ParsedRequest *pr, char *buf,
    terminated by unparse(). If there are no headers, the trailing \r\n is
    unparsed.
  */
-int ParsedRequest_unparse_headers(struct ParsedRequest *pr, char *buf, 
-				  size_t buflen);
+int ParsedRequest_unparse_headers (struct ParsedRequest *pr, char *buf,
+				   size_t buflen);
 
 /* Total length including request line, headers and the trailing \r\n*/
-size_t ParsedRequest_totalLen(struct ParsedRequest *pr);
+size_t ParsedRequest_totalLen (struct ParsedRequest *pr);
 
 /* Length including headers, if any, and the trailing \r\n but excluding the
  * request line. 
  */
-size_t ParsedHeader_headersLen(struct ParsedRequest *pr);
+size_t ParsedHeader_headersLen (struct ParsedRequest *pr);
 
 /* Set, get, and remove null-terminated header keys and values */
-int ParsedHeader_set(struct ParsedRequest *pr, const char * key, 
-		      const char * value);
-struct ParsedHeader* ParsedHeader_get(struct ParsedRequest *pr, 
-				      const char * key);
-int ParsedHeader_remove (struct ParsedRequest *pr, const char * key);
+int ParsedHeader_set (struct ParsedRequest *pr, const char *key,
+		      const char *value);
+struct ParsedHeader *ParsedHeader_get (struct ParsedRequest *pr,
+				       const char *key);
+int ParsedHeader_remove (struct ParsedRequest *pr, const char *key);
 
 /* debug() prints out debugging info if DEBUG is set to 1 */
-void debug(const char * format, ...);
+void debug (const char *format, ...);
 
 /* Example usage:
 
